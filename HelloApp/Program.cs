82e7fbdd2a3c7d8bd1ff11de2f1ceaff1950174f
ieromenko <olga.ieromenko@nixsolutions.com>
2021-11-19 12:37:47 +0200

    2 переменные

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,31 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-int variable = 1;
 int i = 1;
-
-Console.WriteLine($"Значение variable до цикла: {variable}");
-Console.WriteLine($"Значение i до цикла: {i}");
-
-for ( ; ; variable = variable + 10)
+int w = 100; 
+for (; i<33 && w>55 ; i++ )
 {
-    if (variable > 70 && variable < 90)
-    {
-        continue;
-    }
-
-    Console.WriteLine($"Значение variable в цикле: {variable}");
-    Console.WriteLine($"Значение i в цикле: {i}");
-
-    if (variable >= 100)
-    {
-        i = 10;
-    }
-
-    if(i >= 10)
-    {
-        break;
-    }
+    Console.WriteLine($"i = {i}");
+    Console.WriteLine($"w = {w}");
+    w--;
 }
-
-Console.WriteLine($"Значение variable по итогу: {variable}");
-Console.WriteLine($"Значение i по итогу: {i}");
