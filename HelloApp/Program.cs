d28b61fdcd8a8d8d46b0654c9864b211759ea333
ieromenko <olga.ieromenko@nixsolutions.com>
2021-11-19 12:49:02 +0200

    for if

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,9 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int i = 1;
-int w = 100; 
-for (; i<33 && w>55 ; i++ )
+int w = 100;
+for (; i < 33 && w > 55; i++, w--)
 {
     Console.WriteLine($"i = {i}");
     Console.WriteLine($"w = {w}");
-    w--;
 }
