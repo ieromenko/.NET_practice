e6a594ee149520d033874c496a9b8246ac75ea3f
ieromenko <olga.ieromenko@nixsolutions.com>
2021-11-19 11:48:40 +0200

    for loop

---
+++ HelloApp/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int variable = 1;
+int i = 1;
+
+Console.WriteLine($"Значение variable до цикла: {variable}");
+Console.WriteLine($"Значение i до цикла: {i}");
+
+for ( ; ; variable = variable + 10)
+{
+    if (variable > 70 && variable < 90)
+    {
+        continue;
+    }
+
+    Console.WriteLine($"Значение variable в цикле: {variable}");
+    Console.WriteLine($"Значение i в цикле: {i}");
+
+    if (variable >= 100)
+    {
+        i = 10;
+    }
+
+    if(i >= 10)
+    {
+        break;
+    }
+}
+
+Console.WriteLine($"Значение variable по итогу: {variable}");
+Console.WriteLine($"Значение i по итогу: {i}");
