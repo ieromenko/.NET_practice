c1844226e4534072fbda62927783e8d95e874f88
ieromenko <olga.ieromenko@nixsolutions.com>
2021-11-22 16:07:07 +0200

    while

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,23 +1,11 @@
-﻿int[] numbers = { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
-int result = 0;
-foreach (int number in numbers)
-{
-    if (number >= 0)
-    {
-        result++;
-    }
-}
-Console.WriteLine($"Число элементов больше нуля: {result}");
-int n = numbers.Length; // длина массива
-int k = n / 2;          // середина массива
-int temp;               // вспомогательный элемент для обмена значениями
-for (int i = 0; i < k; i++)
-{
-    temp = numbers[i];
-    numbers[i] = numbers[n - i - 1];
-    numbers[n - i - 1] = temp;
-}
-foreach (int i in numbers)
-{
-    Console.Write($"{i} \t");
-}
+﻿        int i = 0;
+        while (i < 20)
+        {
+            if (i == 10)
+            {
+                i++;
+                continue;
+            }
+            Console.WriteLine(i);
+            i++;
+        }
