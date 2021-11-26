eff4b79e212a08f7482abef9590f5884c5cd8a69
ieromenko <olga.ieromenko@nixsolutions.com>
2021-11-26 11:35:12 +0200

    Задание2

---
+++ HelloApp/Task1.cs
@@ -0,0 +1,34 @@
+﻿namespace HelloApp
+{
+    public class Task1
+    {
+        public void Do()
+        {
+            int a, b;
+
+            Console.WriteLine("Введите два числа : ");
+
+            a = Convert.ToInt32(Console.ReadLine());
+
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+
+            {
+
+                Console.WriteLine("{0} наибольшее число", a);
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("{0} наибольшее число ", b);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloApp/study/task2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloApp.study
+{
+    internal class task2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > 5 && num1 < 10)
+            {
+                Console.WriteLine("Число больше 5 и < 10");
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
